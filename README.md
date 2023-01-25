e531978af8baac06a2b487ef6fe608a445f4ba9d
Studeeric <21146632@student.hhs.nl>
2023-01-25 16:31:11 +0100

    gradient and dynamic header

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Portfolio-site
+
+Colourscheme: https://colorhunt.co/palette/222831393e4600adb5eeeeee
+
+Darkener: https://pinetools.com/darken-color
--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,13 +1,22 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
+const items = [
+    "Home",
+    "Card",
+    "Login"
+]
+
 const Header = () => {
+
     return (
         <div className="NavBar">
             <Link to="/"><img className="NavBar__Logo" src="/images/cardImages/SIM-logo.png" /></Link>
             <div className="NavBar__List">
-                <Link className="NavBar__Item" to="/">Home</Link>
-                <Link className="NavBar__Item" to="/card">Card</Link>
+                {items.map((item) => (
+                <Link className="NavBar__Item" to={item === 'Home' ? "/" : item}>{item}</Link>
+                ))}
             </div>
         </div>
     )
