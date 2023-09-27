5598d9a0b9842b35e18fdfbbb6703647913cbb4b
Studeeric <21146632@student.hhs.nl>
2023-09-27 21:26:35 +0200

    Few frontend changes since last time lol

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import Card from './components/card/Card';
 import AppRouter from './components/routing/AppRouter';
-import Home from './components/home/Home';
 import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
-      <Header/>
-      <AppRouter/>
+      <Header />
+      <AppRouter />
     </div>
   );
 }
--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,5 +1,3 @@
-import { render } from "@testing-library/react";
-import React from "react";
 import { Link } from "react-router-dom";
 
 const items = [
@@ -12,10 +10,10 @@ const Header = () => {
 
     return (
         <div className="NavBar">
-            <Link to="/"><img className="NavBar__Logo" src="/images/cardImages/SIM-logo.png" /></Link>
+            <Link to="/"><img className="NavBar__Logo" src="/images/cardImages/SIM-logo.png" alt="Logo"/></Link>
             <div className="NavBar__List">
                 {items.map((item) => (
-                <Link className="NavBar__Item" to={item === 'Home' ? "/" : item}>{item}</Link>
+                <Link key={item} className="NavBar__Item" to={item === 'Home' ? "/" : item}>{item}</Link>
                 ))}
             </div>
         </div>
